aebb8f35bf0afde4f2d0b8f7d892fb1963211dbf
Owen Yap <owen.y.yuhan@gmail.com>
2020-11-24 12:59:56 +0800

    Finished Create task feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,27 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    id = params[:id].to_i
+    @task = Task.find(id)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to index_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index', as: :index
+  get 'show/:id', to: 'tasks#show'
+  get 'task/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
 end
